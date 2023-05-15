51c74f541ce030776d6e01ebc3e2b6c8dc84150c
Votankiet <125332988+Votankiet@users.noreply.github.com>
2023-05-15 15:21:57 +0700

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,46 @@
+package bai1;
+
+public class Product {
+        private String description;
+        private String productID;
+        private double price;
+    
+        public Product() {}
+    
+        public Product(String description, String productID, double price) {
+            this.description = description;
+            this.productID = productID;
+            this.price = price;
+        }
+    
+        public String getDescription() {
+            return description;
+        }
+    
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    
+        public String getProductID() {
+            return productID;
+        }
+    
+        public void setProductID(String productID) {
+            this.productID = productID;
+        }
+    
+        public double getPrice() {
+            return price;
+        }
+    
+        public void setPrice(double price) {
+            this.price = price;
+        }
+    
+        @Override
+        public String toString() {
+            return String.format("%s (Product ID: %s) - $%.2f", description, productID, price);
+        }
+    }
+    
+
